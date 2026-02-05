207376ed54d2af23f171e62061da2b8c488d4c10
Pavel <dreamsobenatic00@mail.ru>
2026-02-05 19:12:37 +0800

    REACT+TS app initialized

---
+++ rms_front/vite.config.ts
@@ -0,0 +1,30 @@
+import path from "path";
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+
+// https://vite.dev/config/
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), "");
+
+    const apiProtocol = env.VITE_API_PROTOCOL || "http";
+    const apiHost = env.VITE_API_HOST || "127.0.0.1";
+    const apiPort = env.VITE_API_PORT || "5000";
+
+    return {
+        plugins: [tailwindcss(), react()],
+        resolve: {
+            alias: {
+                "@": path.resolve(__dirname, "./src"),
+            },
+        },
+        server: {
+            proxy: {
+                "/api": {
+                    target: `${apiProtocol}://${apiHost}:${apiPort}/`,
+                    changeOrigin: true,
+                },
+            },
+        },
+    };
+});
