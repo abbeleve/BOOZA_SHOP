370112324d2dedb538e40825921f9b42e945cba5
Pavel <dreamsobenatic00@mail.ru>
2026-02-08 18:14:54 +0800

    Added footer and header components

--- rms_front/src/pages/MainPage.tsx
+++ rms_front/src/pages/MainPage.tsx
@@ -1,7 +1,26 @@
+import Footer from "@/components /Footer";
+import Header from "@/components /Header";
+
+import { type Link } from "@/types/header";
+
+
+const headerItems: Link[] = [
+    {label: "Меню", link: "/"},
+    {label: "Доставка", link: "/"},
+    {label: "О нас", link: "/"},
+];
+
+const phoneNumber: string = "8 800 555-35-35";
+const mail: string = "dreamsobenatic00@mail.ru"
+
 function MainPage() {
     return (
-        <div>
-            <h1>Ресторан Бууза</h1>
+        <div className="flex flex-col">
+            <Header items={headerItems} phoneNumber={phoneNumber}/>
+            <div>
+                <p>SEPARATOR</p>
+            </div>
+            <Footer mainLinks={headerItems} additionalLinks={[]} mail={mail} phoneNumber={phoneNumber} />
         </div>
     )
 }
